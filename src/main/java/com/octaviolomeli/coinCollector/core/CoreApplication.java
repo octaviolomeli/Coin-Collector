ccd985faf8c8d689bdd05a7e7d120ed99a53c4bb
Octavio Lomeli-Castro <octavio.lomelicastro@gmail.com>
2023-12-29 21:58:29 -0800

    Scripts finished

--- src/main/java/com/octaviolomeli/coinCollector/core/CoreApplication.java
+++ src/main/java/com/octaviolomeli/coinCollector/core/CoreApplication.java
@@ -3,7 +3,7 @@ package com.octaviolomeli.coinCollector.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,8 +14,26 @@ public class CoreApplication {
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
-	@PostMapping("/seed")
-	public void seed(@RequestParam(value = "seed") Long seed, @RequestParam(value = "keyPresses", defaultValue = "") String keyPresses) {
+	@PostMapping("/generateWorld")
+	public void generateWorld(@RequestBody WorldInfo wi) {
+//		Engine newGame = new Engine(wi.getSeed(), wi.getKeyPresses());
+//		newGame.run();
+		System.out.println(wi.getKeyPresses());
+		System.out.println(wi.getSeed());
+	}
 
+	// For mapping JSON to WorldInfo object
+	public static class WorldInfo {
+		private final String seed;
+		private String keyPresses = "";
+
+		public WorldInfo(String s, String k) {
+			this.seed = s;
+			this.keyPresses = k;
+		}
+
+		public String getSeed() { return this.seed; }
+
+		public String getKeyPresses() { return this.keyPresses; }
 	}
 }
--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -1,10 +1,14 @@
+/*
+    Upon loading the page, make a request to the backend for the database entries.
+    For each entry, make a button element that onClick, will generate the corresponding world.
+*/
 window.addEventListener("load", () => {
     fetch("/data")
     .then(response => {
         return response.json();
     })
     .then(slotData => {
-        // create buttons for each saved slot
+        // Create buttons for each saved slot
         const insertElementParent = document.getElementsByClassName("insertHere")[0];
         let c = 0
         for (const row of slotData.data) {
@@ -20,6 +24,7 @@ window.addEventListener("load", () => {
             insertElementParent.appendChild(slotElement);
             c += 1;
         }
+        // If no entries
         if ( c == 0 ) {
             const noSlots = document.createElement('div');
             noSlots.innerText = "There are no saved worlds.";
@@ -32,6 +37,15 @@ window.addEventListener("load", () => {
     })
 });
 
+
+// Make a request to the backend to generate the corresponding world.
 function loadWorld(seed, keyPresses) {
-    console.log(seed + keyPresses);
+    window.location.href = '/ongoing.html';
+    fetch('/generateWorld', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({seed: seed, keyPresses: keyPresses})
+    });
 }
